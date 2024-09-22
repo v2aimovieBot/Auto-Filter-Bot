c60f57563fcb011cd0816ebc28b6ff6da212e61d
v2aimovieBot <v2aimovieBot@gmail.com>
2024-09-22 10:15:54 +0530

    Update info.py

--- info.py
+++ info.py
@@ -120,7 +120,7 @@ if len(BIN_CHANNEL) == 0:
 else:
     BIN_CHANNEL = int(BIN_CHANNEL)
 URL = environ.get("URL", "https://federal-ailyn-v2aimoviebotx-028c7545.koyeb.app/")
-if len(URL) == 0:
+if len(URL) == 1:
     print('Error - URL is missing, exiting now')
     exit()
 else:
