7a762dea49b6334f0d3336274238a589fefaffa5
v2aimovieBot <v2aimovieBot@gmail.com>
2024-09-23 07:15:40 +0530

    Update info.py

--- info.py
+++ info.py
@@ -119,7 +119,7 @@ if len(BIN_CHANNEL) == 0:
     exit()
 else:
     BIN_CHANNEL = int(BIN_CHANNEL)
-URL = environ.get("URL", "")
+URL = environ.get("URL", "doubtful-aeriela-v2aimoviebotx-4535a705.koyeb.app/")
 if len(URL) == 0:
     print('Error - URL is missing, exiting now')
     exit()
@@ -128,7 +128,7 @@ else:
         if not URL.endswith("/"):
             URL += '/'
     elif is_valid_ip(URL):
-        URL = f'http://doubtful-aeriela-v2aimoviebotx-4535a705.koyeb.app/'
+        URL = f'http://{URL}/'
     else:
         print('Error - URL is not valid, exiting now')
         exit()
